1e5df90cd01d96462a828790d3aac6e2f2d5dfc8
Payal <poojapayal300@gmail.com>
2026-08-26 23:20:58 +0530

    Keep only forlookbeautyanimation

--- script.js
+++
@@ -1,7 +0,0 @@
-const btnE1=document.querySelector(".btn")
-btnE1.addEventListener("mouseover",(e)=>{
-    const x=e.pageX-btnE1.offsetleft;
-    const y=e.pageY-btnE1.offsetop;
-btnE1.style .setproperty("--posX", x+ "px")
-btnE1.style.setproperty("--posY", y + "px")
-})
